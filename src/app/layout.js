0c54d9dcf7225f07e69ccd06879be0477bfd6a24
Sara Mukesh Kumar <saramukeshkumar@gmail.com>
2025-09-03 12:46:16 +0530

    updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,25 +21,22 @@
 //     </html>
 //   );
 // }
-
-
-// app/layout.js
 "use client";
-
-import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/header";
-import Footer from "@/components/footer";
+import FooterSection from "@/components/footer";
+import ClientClerkProvider from "@/components/ClientClerkProvider";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className="flex flex-col min-h-screen">
-          <Header />
+    <html lang="en">
+      <body className="flex flex-col min-h-screen antialiased bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee]">
+        <ClientClerkProvider>
+          <Header /> {/* SignedIn/SignedOut safe here */}
           <main className="flex-grow">{children}</main>
-          <Footer />
-        </body>
-      </html>
-    </ClerkProvider>
+          <FooterSection />
+        </ClientClerkProvider>
+      </body>
+    </html>
   );
 }
